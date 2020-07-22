da1c9f1b4eabb6c426fdf8b84b2cec2cb21c4eaf
José Lucas Alves da Silva <lukasalvespod1@gmail.com>
2020-07-22 18:09:31 -0300

    understanding the base features, syntax and hooks for state manipulation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Person from "./Person/Person";
 
-function App() {
+const App = (props) => {
+
+  const [personsState, setPersonsState] = useState({
+    persons: [
+      { name: "João", age: 20 },
+      { name: "Maria", age: 18 },
+      { name: "José", age: 22 }
+    ]
+  })
+
+  // const [otherState, setOtherState] = useState('some other value');
+
+  const [showPersons, setShowPersons] = useState(false);
+
+  const switchNameHandler = (newName) => {
+    setPersonsState({
+      persons: [
+        { name: newName, age: 21 },
+        { name: "Maria", age: 19 },
+      ],
+      // otherState: personsState.otherState
+    })
+  }
+
+  const togglePersonsHandler = () => {
+    const doesShow = showPersons;
+    setShowPersons(!doesShow);
+  }
+
+  const nameChangeHandler = (event) => {
+    setPersonsState({
+      persons: [
+        { name: "Lucas", age: 21 },
+        { name: event.target.value, age: 18 }
+      ],
+    })
+  }
+
+  const style = {
+    backgroundColor: 'white',
+    font: 'inherit',
+    border: '1px solid blue',
+    padding: '8px',
+    cursor: 'pointer'
+  }
+
+  let persons = null;
+
+  if (showPersons) {
+    persons = (
+      <div>
+        {personsState.persons.map(person => {
+          return (
+            <Person name={person.name} age={person.age}/>
+          )
+        })}
+        {/* <Person
+          name={personsState.persons[0].name}
+          age={personsState.persons[0].age}
+        />
+        <Person
+          name={personsState.persons[1].name}
+          age={personsState.persons[1].age}
+          click={switchNameHandler.bind(this, 'Lucas!')}
+          changed={nameChangeHandler}
+        >
+          My Hobbies: Signing
+          </Person>
+          <Person
+            name={personsState.persons[2].name}
+            age={personsState.persons[2].age}
+          /> */}
+      </div>
+    )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hi, I'm React APP</h1>
+      <button
+        style={style}
+        onClick={togglePersonsHandler}>
+        Toggle Persons
+      </button>
+      {persons}
+
     </div>
   );
 }
